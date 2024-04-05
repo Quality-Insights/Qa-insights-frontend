a3735e73cb5f012738da3dc2c3af22468f90b36e
Suraj Khayamali <surajkhayamali@lftechnology.com>
2024-04-05 20:23:29 +0545

    Parse tests

--- util.js
+++ util.js
@@ -1,31 +1,53 @@
-const jmespath = require("jmespath");
 const { randomUUID } = require("crypto");
 
 function prepareResult(cypressResult) {
-    const buildStats = {
-      buildid: randomUUID(),  // Using a random uuid as buildId (buildId not found in logs.)
-      startdatetime: jmespath.search(cypressResult, "stats.start"),
-      enddatetime: jmespath.search(cypressResult, "stats.end"),
-      duration: jmespath.search(cypressResult, "stats.duration"),
-      suits: jmespath.search(cypressResult, "stats.suites"),
-      tests: jmespath.search(cypressResult, "stats.tests"),
-      passescount: jmespath.search(cypressResult, "stats.passes"),
-      pendingcount: jmespath.search(cypressResult, "stats.pending"),
-      failurecount: jmespath.search(cypressResult, "stats.failures"),
-      testregisteredcount: jmespath.search(cypressResult, "stats.testsRegistered"),
-      passpercent: jmespath.search(cypressResult, "stats.passPercent"),
-      pendingpercent: jmespath.search(cypressResult, "stats.pendingPercent"),
-      skippedcount: jmespath.search(cypressResult, "stats.skipped"),
-      buildtype: "cypress"
-    };
+  const { stats, results } = cypressResult;
   
-    const payload = {
-      buildStats,
+  const buildid = randomUUID();  // Using a random uuid as buildId (buildId not found in logs.)
+  const buildStats = {
+    buildid: buildid,
+    startdatetime: stats.start,
+    enddatetime: stats.end,
+    duration: stats.duration,
+    suits: stats.suites,
+    tests: stats.tests,
+    passescount: stats.passes,
+    pendingcount: stats.pending,
+    failurecount: stats.failures,
+    testregisteredcount: stats.testsRegistered,
+    passpercent: stats.passPercent,
+    pendingpercent: stats.pendingPercent,
+    skippedcount: stats.skipped,
+    buildtype: "cypress"
+  };
+
+  const tests = [];
+
+  for(const result of results) {
+    const { file, suites } = result;
+
+    for(const suite of suites) {
+      const { uuid, title, duration } = suite;
+
+      const test = {
+        uuid: uuid,
+        title: title,
+        duration: duration,
+        file: file,
+        build_id: buildid
+      }
+      tests.push(test);
     }
-  
-    return payload;
   }
 
-  module.exports = {
-    prepareResult
+  const payload = {
+    buildStats,
+    tests,
   }
+
+  return payload;
+}
+
+module.exports = {
+  prepareResult
+}
