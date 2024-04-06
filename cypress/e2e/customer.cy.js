bae01f74ed6c9d19edf9d3c26f3cdd55b04faf2e
Suraj Khayamali <surajkhayamali@lftechnology.com>
2024-04-07 00:52:09 +0545

    fix: uncommenting the customer test file.

--- cypress/e2e/customer.cy.js
+++ cypress/e2e/customer.cy.js
@@ -1,7 +1,6 @@
 describe("Navigate to each customer", () => {
    // test for each customer
    it("should be able to see title of each customer and not see site under maintainenance", () => {
-      //   console.log("hello");
       customerData.customers.forEach((customer) => {
          cy.visit(customer.url);
 
